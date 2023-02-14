559167b99249b20e75d4f205464c26928b78c49e
MRLRSX <lucas.rocha-dev@outlook.com>
2023-02-14 19:39:01 -0300

    Encriptação Assimetrica

---
+++ encriptacaoAssimetrica/encriptacaoAssimetrica.js
@@ -0,0 +1,33 @@
+import { generateKeyPairSync } from 'crypto';
+
+const { privateKey, publicKey } = generateKeyPairSync('rsa', {
+    modulusLength: 2048,
+
+    publicKeyEncoding:{
+        type: 'spki',
+        format: 'pem',
+    },
+    privateKeyEncoding:{
+        type: 'pkcs8',
+        format: 'pem',
+    },
+});
+
+//console.log(publicKey);
+//console.log(privateKey);
+
+import { publicEncrypt, privateDecrypt } from 'crypto';
+
+const dadosCriptografados = publicEncrypt(
+    publicKey,
+    Buffer.from('Mensagem super secreta')
+);
+
+console.log(dadosCriptografados.toString('hex'));
+
+const dadosDecriptografados = privateDecrypt(
+    privateKey,
+    dadosCriptografados
+);
+
+console.log(`Dados decriptografados ${dadosDecriptografados.toString('utf-8')}`);
